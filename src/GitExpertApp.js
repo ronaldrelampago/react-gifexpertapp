2a6d5cd41e3cd70961c5784ededae0322911d798
Ronald Ortega <ronaldortega490@gmail.com>
2022-05-23 13:55:17 -0400

    First commit

---
+++ src/GitExpertApp.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GitExpertApp = () => {
+    
+    const [categories, setCategories] = useState(['Dragon Ball']);
+
+    return(
+    <>
+        <h2>GitExpertApp</h2>
+        <AddCategory setCategories={setCategories}/>
+        <hr />
+        <ol>
+            {
+                categories.map( category =>(
+                    <GifGrid 
+                        key={ category }
+                        category={category} />
+                ))}
+        </ol>
+
+    </>
+  )
+}
